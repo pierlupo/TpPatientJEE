f7226064aa198ecfce75d63b42e632cd9854db6d
Kraimps Pierre <pier_lupo@laposte.net>
2023-06-12 14:12:10 +0200

    wip

--- src/main/java/com/example/tppatientjee/util/Definition.java
+++ src/main/java/com/example/tppatientjee/util/Definition.java
@@ -2,5 +2,5 @@ package com.example.tppatientjee.util;
 
 public class Definition {
     public static final String VIEW_PATH = "/WEB-INF/views/";
-    public static final String BASE_URL = "http://localhost:8080/correctionpatient_war_exploded";
+    public static final String BASE_URL = "http://localhost:8080/TpPatientJEE_war_exploded";
 }
